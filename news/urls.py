4e9823d6267c4bb2998530f65b0da5b492286b42
dunyung1 <dshege4@gmail.com>
2019-03-06 20:58:55 +0300

    create the URLConf for view. news/urls.py

--- news/urls.py
+++ news/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
   url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name = 'pastnews'),
   url(r'^search/',views.search_results, name='search_results'),   #create a new URLpattern that references the search_results
   url(r'^article/(\d+)',views.article,name ='article')   #create an article route and we capture an integer which will be the id of the article.
+  url(r'^new/article$', views.new_article, name='new-article')
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
